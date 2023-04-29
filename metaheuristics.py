9c8e2ed9c72121ee704804942f65b990e3d8bf11
viniciustmgs <viniciustmgs@gmail.com>
2023-04-29 12:32:01 -0300

    Hill Climbing funcionando

--- metaheuristics.py
+++ metaheuristics.py
@@ -3,7 +3,6 @@ import math
 
 # criando a classe de cada F.O.
 
-
 class Function1:
     def __init__(self) -> None:
         pass
@@ -24,14 +23,46 @@ class Function2:
 # geração de solução inicial
 
 def generateInitialSolution (domain):
-    initialSolution = [random.uniform(domain[0], domain[1]), random.uniform(domain[0], domain[1])]
-    return initialSolution
+    solution = [random.uniform(domain[0], domain[1]), random.uniform(domain[0], domain[1])]
+    return solution
 
 # Hill-Climbing
 
+def hillClimbingNoise (solution, domain):
+    newSolution = []
+    for i in range(2):
+        isFinished = False
+        noiseRange = math.fabs(solution[i]*0.05)
+        #TEM QUE ARRUMAR O RANGE
+        while(isFinished == False):
+            noise = random.uniform(0, noiseRange)
+            modification = random.uniform(-noise, noise)
+            if (solution[i] + modification >= domain[0] and solution[i] + modification <= domain[1]):
+                newSolution.append(solution[i] + modification)
+                isFinished = True
+                print("adicionou noise")
+    return newSolution
+
 def hillClimbing (function, domain):
-    initialSolution = generateInitialSolution(domain)
-    
+    solution = generateInitialSolution(domain)
+    result = function.calculate(solution[0], solution[1])
+    print("X: ", solution[0], "Y: ", solution[1])
+    print("inicial: ", result)
+    limitCounter = 0
+    while(limitCounter < 30):
+        modifiedSolution = hillClimbingNoise(solution, domain)
+        modifiedResult = function.calculate(modifiedSolution[0], modifiedSolution[1])
+        print("X: ", modifiedSolution[0], "Y: ", modifiedSolution[1])
+        print("modificado: ", modifiedResult)
+        if (modifiedResult < result):
+            result = modifiedResult
+            limitCounter = 0
+            print("alterou")
+        else:
+            limitCounter += 1
+            print("não melhorou", limitCounter)
+    return result        
+
 # ILS
 
 def iteratedLocalSearch(function, domain):
@@ -65,5 +96,6 @@ for i in range(4):
     resultsILS.append(executionILS)
 
 # valores finais serão salvos em vetores, no final exibir o resultado final e plotar em boxplot
-print(resultsHillClimbing)
-print(resultsILS)
+#print(resultsHillClimbing)
+#print(resultsILS)
+print(hillClimbing(function1, domains[0]))
