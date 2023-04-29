6c40643cf5bb6d66152811d0b5d5916ec967b8a1
viniciustmgs <viniciustmgs@gmail.com>
2023-04-29 19:54:35 -0300

    Hill Climbing funcionando!

--- metaheuristics.py
+++ metaheuristics.py
@@ -20,7 +20,7 @@ class Function2:
         result = -(y+47)*math.sin(math.sqrt(math.fabs(y+0.5*y+47))) - x*math.sin(math.sqrt(math.fabs(x-(y+47))))
         return result
     
-# geração de solução inicial
+# gerador de solução inicial
 
 def generateInitialSolution (domain):
     solution = [random.uniform(domain[0], domain[1]), random.uniform(domain[0], domain[1])]
@@ -30,44 +30,38 @@ def generateInitialSolution (domain):
 
 def hillClimbingNoise (solution, domain):
     newSolution = []
+    noiseRange = math.fabs((domain[1]-domain[0])*0.01)
     for i in range(2):
         isFinished = False
-        noiseRange = math.fabs(solution[i]*0.05)
-        #TEM QUE ARRUMAR O RANGE
         while(isFinished == False):
             noise = random.uniform(0, noiseRange)
             modification = random.uniform(-noise, noise)
             if (solution[i] + modification >= domain[0] and solution[i] + modification <= domain[1]):
                 newSolution.append(solution[i] + modification)
                 isFinished = True
-                print("adicionou noise")
     return newSolution
 
-def hillClimbing (function, domain):
+def hillClimbing (function, domain, stopCondition):
     solution = generateInitialSolution(domain)
     result = function.calculate(solution[0], solution[1])
-    print("X: ", solution[0], "Y: ", solution[1])
-    print("inicial: ", result)
     limitCounter = 0
-    while(limitCounter < 30):
+    while(limitCounter < stopCondition):
         modifiedSolution = hillClimbingNoise(solution, domain)
         modifiedResult = function.calculate(modifiedSolution[0], modifiedSolution[1])
-        print("X: ", modifiedSolution[0], "Y: ", modifiedSolution[1])
-        print("modificado: ", modifiedResult)
         if (modifiedResult < result):
+            solution = modifiedSolution
             result = modifiedResult
             limitCounter = 0
-            print("alterou")
         else:
             limitCounter += 1
-            print("não melhorou", limitCounter)
-    return result        
+    return [solution, result]       
 
 # ILS
 
 def iteratedLocalSearch(function, domain):
     initialSolution = generateInitialSolution(domain)
-    #faz um hill climbing na solução inicial
+    solution = hillClimbing(function, domain, 100)[0]
+    
 
 # iniciando a execução das meta-heurísticas em um loop, cada uma das 2 é executada
 # 30 vezes para cada um dos domínios 
@@ -85,17 +79,24 @@ for i in range(4):
         if (i < 2):
             # entra nesse if se for os domínios A e B, portanto executa as meta-heurísticas
             # para a função 1
-            executionHillClimbing.append(j)
+
+            #executionHillClimbing.append(hillClimbing(function1, domains[i], 100))
             executionILS.append(j)
         else:
             # entra nesse if se for os domínios C e D, portanto executa as meta-heurísticas
             # para a função 2
-            executionHillClimbing.append(j)
+            
+            #executionHillClimbing.append(hillClimbing(function2, domains[i], 100))
             executionILS.append(j)
-    resultsHillClimbing.append(executionHillClimbing)
-    resultsILS.append(executionILS)
+    #resultsHillClimbing.append(executionHillClimbing)
+    #resultsILS.append(executionILS)
 
 # valores finais serão salvos em vetores, no final exibir o resultado final e plotar em boxplot
 #print(resultsHillClimbing)
 #print(resultsILS)
-print(hillClimbing(function1, domains[0]))
+#print("dominio A:\n", resultsHillClimbing[0])
+#print("dominio B:\n", resultsHillClimbing[1])
+#print("dominio C:\n", resultsHillClimbing[2])
+#print("dominio D:\n", resultsHillClimbing[3])
+iteratedLocalSearch(function1, domains[0])
+
